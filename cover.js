68830c84c0ca5e853311fba045b2bb12be615be4
Andres Aravena <andres.aravena@gmail.com>
2019-04-03 12:51:34 +0300

    prepare for color picking

--- cover.js
+++ cover.js
@@ -13,7 +13,7 @@ class Face {
     this.fg_color="#00000";
     this.fontFamily="";
     this.fontSize=fontSize;
-    this.lineHeight="20mm";
+    this.lineHeight=fontSize;
   };
 
   set text(value) {
@@ -54,7 +54,7 @@ class Face {
   }
 
   set lineHeight(value) {
-    this._fontSize = value;
+    this._lineHeight = value;
     this.div.style.lineHeight = value;
   }
 
@@ -120,6 +120,15 @@ class Input {
     this.input.setAttribute("name", action);
     return(this);
   }
+  link(text, href, action) {
+    let anchore = document.createElement("a");
+    anchore.innerHTML = text;
+    anchore.setAttribute("href", href);
+    anchore.setAttribute("onclick", action);
+    this.div.appendChild(anchore);
+    return(this);
+  }
+  
 }
 
 class Pane {
@@ -143,6 +152,7 @@ class Pane {
       .onchange(tgt.id + ".fileSelect(this)").div);
     this.col1.appendChild(new Input("bg", "color")
       .Label("Background").value(tgt._bg_color)
+      .link("Pick Color", "#", "createImage('source','title','background')")
       .onchange(tgt.id + ".bg_color=this.value").div);
     this.col2.appendChild(new Input("fg", "color")
       .Label("Foreground").value(tgt._fg_color)
@@ -154,7 +164,7 @@ class Pane {
       .Label("Height").value(tgt._height)
       .onchange(tgt.id + ".height=this.value").div);
     this.col1.appendChild(new Input("lineSize", "text")
-      .Label("Line Size").value(tgt._lineHeight)
+      .Label("Line Height").value(tgt._lineHeight)
       .onchange(tgt.id + ".lineHeight=this.value").div);
     this.col2.appendChild(new Input("fontSize", "text")
       .Label("Font Size").value(tgt._fontSize)
@@ -190,9 +200,88 @@ class Pane {
   }
 };
 
-front = new Face("front", "", "100mm", "131mm","20mm");
-back = new Face("back", "", "100mm", "131mm","10mm");
-side = new Face("side", "Book Title", "20mm", "131mm","20mm");
+class ColorPicker {
+    function componentToHex(c) {
+      var hex = c.toString(16)
+      return hex.length == 1 ? "0" + hex : hex
+    }
+
+    function rgbToHex(r, g, b) {
+      return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b)
+    }
+
+    function drawCrosshair(r, g, b) {
+      var ctx = document.getElementById('crosshair').getContext('2d')
+      ctx.strokeStyle = 'rgba(' + r + ',' + g + ',' + b + ',0.25)'
+      ctx.beginPath()
+      ctx.moveTo(5.5, 0)
+      ctx.lineTo(5.5, 4)
+      ctx.stroke()
+      ctx.beginPath()
+      ctx.moveTo(7, 5.5)
+      ctx.lineTo(11, 5.5)
+      ctx.stroke()
+      ctx.beginPath()
+      ctx.moveTo(5.5, 7)
+      ctx.lineTo(5.5, 11)
+      ctx.stroke()
+      ctx.beginPath()
+      ctx.moveTo(0, 5.5)
+      ctx.lineTo(4, 5.5)
+      ctx.stroke()
+    }
+
+    // “Update details” button opens the <dialog> modally
+    function createImage(canvas_id, tab_id, attr_id) {
+      // info.onmouseleave = function() {
+      //   $(this).find('input').blur()
+      // };
+
+      const zoom = 3;
+      // var tab = document.getElementById(tab_id);
+      var canvas = document.getElementById(canvas_id);
+      var ctx = canvas.getContext('2d');
+      let img = new Image()
+    img.onload = function() {
+      zoomed.width = img.width * zoom
+      zoomed.height = img.height * zoom
+      ztx.imageSmoothingEnabled = false
+      ztx.mozImageSmoothingEnabled = false
+      ztx.webkitImageSmoothingEnabled = false
+      // ztx.putImageData(img, 0, 0)
+      ztx.drawImage(img, 0, 0, zoomed.width, zoomed.height)
+
+      info.style.display = "block";
+      var hex;
+      canvas.onclick = function(e) {
+    	changeAttribute(tab_id,attr_id, hex)
+      }
+      canvas.onmousemove = function(e) {
+        var x = e.offsetX/canvas.clientWidth *canvas.width,
+    	y = e.offsetY/canvas.clientHeight*canvas.height,
+    	d = ctx.getImageData(x, y, 1, 1).data,
+    	r = d[0], g = d[1], b = d[2],
+    	ir = 255 - r, ig = 255 - g, ib = 255 - b,
+    	inv = rgbToHex(ir, ig, ib);
+
+        hex = rgbToHex(r, g, b);
+        // console.log( x + ' x ' + y, hex);
+        coord.innerHtml = x + ' x ' + y;
+        coord.style.color= inv;
+        hex_hover.value = hex;
+        rgb_hover.value = r + ',' + g + ',' + b;
+        color_hover.style.background= hex;
+
+        zoomed.style.left= (-(zoom * x) + (zoomer.clientWidth)/2)+"px";
+        zoomed.style.top = (-(zoom * y) + (zoomer.clientHeight)/2)+"px";
+
+        drawCrosshair(ir, ig, ib)
+      };
+    }
+      img.src=canvas.toDataURL()
+    }
+
+}
 
 function showBlock(dst) {
   var doc = document.getElementById(dst);
@@ -205,3 +294,11 @@ function showBlock(dst) {
     tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
   }
 }
+
+front = new Face("front", "", "100mm", "131mm","20mm");
+back = new Face("back", "", "100mm", "131mm","10mm");
+side = new Face("side", "Book Title", "20mm", "131mm","20mm");
+
+front_tab=new Pane("front-tab", front)
+back_tab=new Pane("back-tab", back)
+side_tab=new Pane("center-tab", side)
